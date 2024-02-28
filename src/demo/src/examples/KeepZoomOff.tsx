7ed05461c32dc323f708f614ddfc35c8dcfa1525
Michal Kukla <michal.kukla91@gmail.com>
2024-02-28 12:08:26 +0100

    more content added

--- src/demo/src/examples/KeepZoomOff.tsx
+++ src/demo/src/examples/KeepZoomOff.tsx
@@ -1,7 +1,8 @@
-import { CSSProperties } from "react";
+import { CSSProperties, useState } from "react";
 import usePinchZoom from "use-pinch-zoom";
 
 export default function KeepZoomOff() {
+  const [activeIndex, setActiveIndex] = useState(0);
   const {
     pinchZoomTransitionX,
     pinchZoomTransitionY,
@@ -12,23 +13,49 @@ export default function KeepZoomOff() {
     onDraging,
     onTouchEnd,
     isZooming,
-    onMouseWheel,
   } = usePinchZoom({
     maxZoom: 7,
     keepZoom: false,
   });
 
-  const containerStyles: CSSProperties = {
-    display: "flex",
+  const elements = [
+    {
+      src: "https://cdn.pixabay.com/photo/2020/07/15/22/07/instagram-5409107_1280.jpg",
+      text: "Instagram was launched on October 6, 2010, and gained 25,000 users on its first day.",
+    },
+    {
+      src: "https://cdn.pixabay.com/photo/2014/03/22/22/17/phone-292994_1280.jpg",
+      text: "Initially, Instagram was called Codename, but the name changed to Instagram before it was launched.",
+    },
+    {
+      src: "https://cdn.pixabay.com/photo/2021/06/25/12/26/social-media-6363633_1280.jpg",
+      text: "The name Instagram comes from combining “instant camera” and “telegram.",
+    },
+    {
+      src: "https://cdn.pixabay.com/photo/2019/08/19/07/45/corgi-4415649_1280.jpg",
+      text: "The first photo ever posted was by the co-founder, @kevin, on July 16, 2010. The photo is a picture of a dog.",
+    },
+    {
+      src: "https://cdn.pixabay.com/photo/2018/03/30/15/10/skyscraper-3275591_1280.jpg",
+      text: "New York City was the most Instagrammed city in 2017. However, the most Instagrammed location was Disneyland in Anaheim, California.",
+    },
+  ];
+
+  const wrapperStyles: CSSProperties = {
+    paddingTop: "150px",
+    position: "relative",
     width: "100vw",
     maxWidth: "100%",
+    touchAction: "pan-y",
+    overflow: "hidden",
+  };
+
+  const containerStyles: CSSProperties = {
+    display: "flex",
+    width: "100%",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    marginTop: "170px",
-    marginBottom: "200px",
-    height: "70vh",
-    touchAction: "pan-y",
   };
 
   const imgContainerStyles: CSSProperties = {
@@ -39,6 +66,7 @@ export default function KeepZoomOff() {
   };
 
   const imgStyles: CSSProperties = {
+    position: "relative",
     alignSelf: "center",
     display: "block",
     height: "auto",
@@ -46,37 +74,55 @@ export default function KeepZoomOff() {
     maxHeight: "100%",
     maxWidth: "100%",
     transition: isDragging || isZooming ? "0s" : "0.3s",
-    transform: `scale(${zoom}) translateX(${pinchZoomTransitionX}px) translateY(${pinchZoomTransitionY}px)`,
   };
 
   const textContainerStyles: CSSProperties = {
     paddingLeft: "5%",
     paddingRight: "5%",
-    paddingTop: "2%",
+    paddingBottom: "2%",
     flexBasis: "8%",
   };
 
   return (
     <>
-      <section style={containerStyles}>
-        <div style={imgContainerStyles}>
-          <img
-            onMouseDown={onMouseDown}
-            onTouchMove={onDraging}
-            onTouchEnd={onTouchEnd}
-            onTouchStart={onDragStart}
-            onWheel={onMouseWheel}
-            style={imgStyles}
-            src="https://cdn.pixabay.com/photo/2020/07/15/22/07/instagram-5409107_1280.jpg"
-          />
-        </div>
+      <section style={wrapperStyles}>
+        {elements.map((item, index) => (
+          <article key={index} style={containerStyles}>
+            <div
+              onTouchStart={() => {
+                setActiveIndex(index);
+              }}
+              style={imgContainerStyles}
+            >
+              <img
+                onMouseDown={activeIndex === index ? onMouseDown : undefined}
+                onTouchMove={activeIndex === index ? onDraging : undefined}
+                onTouchEnd={activeIndex === index ? onTouchEnd : undefined}
+                onTouchStart={activeIndex === index ? onDragStart : undefined}
+                style={{
+                  zIndex: index === activeIndex ? 2 : 1,
+                  transform:
+                    index === activeIndex
+                      ? `scale(${zoom}) translateX(${pinchZoomTransitionX}px) translateY(${pinchZoomTransitionY}px)`
+                      : "",
+                  ...imgStyles,
+                }}
+                src={item.src}
+                alt={`image ${index}`}
+              />
+            </div>
 
-        <div style={textContainerStyles}>
-          <h1 style={{ margin: "0" }}>
-            Instagram was launched on October 6, 2010, and gained 25,000 users
-            on its first day.
-          </h1>
-        </div>
+            <div style={textContainerStyles}>
+              <h1 style={{ margin: "0" }}>{item.text}</h1>
+            </div>
+          </article>
+        ))}
+        <p style={{ textAlign: "center", padding: "5%" }}>
+          source:{" "}
+          <a href="https://www.thefactsite.com/instagram-facts">
+            https://www.thefactsite.com/instagram-facts
+          </a>
+        </p>
       </section>
     </>
   );
